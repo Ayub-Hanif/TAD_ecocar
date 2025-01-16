6da4828a553e1bb26018dc068d5e999826d18d4a
Mohammad Ayub <ayubhanif06@gmail.com>
2025-01-16 11:42:54 -0800

    Update run script to point to the correct Streamlit application path

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd /home/ucd-ecocar/TAD/venv/
-/home/ucd-ecocar/TAD/venv/bin/streamlit run src/main.py --server.headless true --server.port 8080
+/home/ucd-ecocar/TAD/venv/bin/streamlit run application/main.py --server.headless true --server.port 8080
